e090abc51a880cf46f356eef8a731f23e849df23
Simon McKenna <sshnug.si+github@gmail.com>
2022-02-05 14:42:05 +1030

    Fix mail links

--- src/sections/contact.js
+++ src/sections/contact.js
@@ -24,19 +24,19 @@ const ContactSection = () => (
         <Committee
           position="Secretary"
           name="Justin 'Astromek' Grubb"
-          email="secretary@adelaidefpvracing.com.au"
+          email="mailto:secretary@adelaidefpvracing.com.au"
         />
         <Committee
           position="Treasurer"
           name="David 'CrazyTrain' Hoyle"
-          email="treasurer@adelaidefpvracing.com.au"
+          email="mailto:treasurer@adelaidefpvracing.com.au"
         />
       </div>
       <div className="field half">
         <Committee
           position="Events Manager"
           name="Pat 'Anox' Purcell"
-          email="events@adelaidefpvracing.com.au"
+          email="mailto:events@adelaidefpvracing.com.au"
         />
         <Committee
           position="Safety Officer"
